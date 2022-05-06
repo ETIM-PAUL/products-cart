c8a9ff4fde87f0b0a006ce79217a66984dcc029e
ETIM-PAUL <etimpaul22@gmail.com>
2022-05-06 03:58:28 +0100

    removing deleted files

--- src/redux/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from "redux";
-import { productsReducer, cartReducer, } from "./productsReducer";
-
-export const reducer = combineReducers({
-  action: productsReducer,
-  cart:cartReducer,
-});
